c1a5fa0efbb0ff1ada34c2bd2d4c34dd96ce0f7d
jiaul.jibon <jiaul.jibon@strativ.se>
2025-08-04 09:22:56 +0600

    Expand exports in index.ts to include individual error and middleware modules

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-export * from './errors';
-export * from './middlewares';
+export * from './errors/base-error';
+export * from './errors/database-error';
+export * from './errors/not-found-error';
+export * from './errors/not-authenticated-error';
+export * from './errors/validation-error';
+export * from './errors/request-validation-error';
+export * from "./errors/object-already-exists";
+
+
+export * from './middlewares/error-handler-middleware';
+export * from './middlewares/validate-request-middleware';
+export * from './middlewares/current-user-middleware';
+export * from './middlewares/require-auth-middleware';
+export * from './middlewares/validate-request-middleware';
+
