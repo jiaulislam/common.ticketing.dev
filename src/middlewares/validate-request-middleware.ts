b245a072749e0a2ad4f7f9752ad7d13c08ab2082
jiaul.jibon <jiaul.jibon@strativ.se>
2025-08-04 11:10:16 +0600

    Simplify request validation in middleware by removing redundant type checks

--- src/middlewares/validate-request-middleware.ts
+++ src/middlewares/validate-request-middleware.ts
@@ -1,12 +1,12 @@
 import {Request, Response, NextFunction} from "express";
-import {validationResult, FieldValidationError, ValidationError} from "express-validator";
+import {validationResult, FieldValidationError, Result} from "express-validator";
 import {RequestValidationError} from "../errors";
 
 
 export const validateRequestMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  const errors = validationResult(req);
+  const errors = validationResult(req) as Result<FieldValidationError>;
   if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array().filter((err: ValidationError): err is FieldValidationError => err.type === 'field'));
+      throw new RequestValidationError(errors.array());
   }
   next();
 }
