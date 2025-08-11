b0e05746b0b0874156dc6a3a5fae496975e8e55f
jiaul.jibon <jiaul.jibon@strativ.se>
2025-08-11 12:34:38 +0600

    refactoring codebase

--- src/errors/validation-error.ts
+++ src/errors/validation-error.ts
@@ -5,8 +5,9 @@ export class ValidationError extends CustomError {
     public statusCode: number = status.NOT_FOUND;
     reason = 'Validation error';
 
-    constructor() {
-        super('Validation error');
+    constructor(reason: string) {
+        super(reason);
+        this.reason = reason;
         // Set the prototype explicitly.
         Object.setPrototypeOf(this, ValidationError.prototype);
     }
